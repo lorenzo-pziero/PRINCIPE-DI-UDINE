f050dfa9d1f1465eb89f8e54599b2422c553536c
Lorenzo Peruzzo Ziero <91576172+lorenzo-pziero@users.noreply.github.com>
2021-11-28 22:58:07 -0300

    Create principe.php
    -> Cardapio/menu do Restaurante Principe Di Udine; situado em Bento Gonçalves.

---
+++ principe.php
@@ -0,0 +1,352 @@
+<?php
+$principediudine = "PRINCIPE DI UDINE";
+function show() {	
+	global $principediudine;
+	echo $principediudine;
+}
+show();
+echo "<br/>";
+$pf = "PRATO FEITO -> R$ 59.00 ao KG";
+	function show1() {
+		global $pf;
+		echo $pf;
+}
+show1();
+echo "<br/>";
+$freelunch = "PRATO LIVRE -> R$ 70.00";
+	function show2() {
+		global $freelunch;
+		echo $freelunch;
+}
+show2();
+echo "<br/>";
+$porcaofritas = "FRITAS -> R$ 20.00";
+	function show3() {
+		global $porcaofritas;
+		echo $porcaofritas;
+}
+show3();
+echo "<br/>";
+$salsicha = "SALSICHA -> R$ 30.00";
+	function show4() {
+		global $salsicha;
+		echo $salsicha;
+}
+show4();
+echo "<br/>";
+$pastelcarne = "PASTEL DE CARNE -> R$ 10.00";
+	function show5() {
+		global $pastelcarne;
+		echo $pastelcarne;
+}
+show5();
+echo "<br/>";
+$pastelqueijo = "PASTEL DE QUEIJO -> R$ 10.00";
+	function show6() {
+		global $pastelqueijo;
+		echo $pastelqueijo;
+}
+show6();
+echo "<br/>";
+$pastelsiri = "PASTEL DE SIRI -> R$ 10.00";
+	function show7() {
+		global $pastelsiri;
+		echo $pastelsiri;
+}
+show7();
+echo "<br/>";
+$torrada = "TORRADA -> R$ 30.00";
+	function show8() {
+		global $torrada;
+		echo $torrada;
+}
+show8();
+echo "<br/>";
+$torradacomfritas = "TORRADA COM FRITAS -> R$ 45.00";
+	function show9() {
+		global $torradacomfritas;
+		echo $torradacomfritas;
+}
+show9();
+echo "<br/>";
+$xisfilebaccon = "XIS FILE COM BACCON -> R$ 40.00";
+	function show10() {
+		global $xisfilebaccon;
+		echo $xisfilebaccon;
+}
+show10();
+echo "<br/>";
+$xisfrango = "XIS FRANGO -> R$ 40.00";
+	function show11() {
+		global $xisfrango;
+		echo $xisfrango;
+}
+show11();
+echo "<br/>";
+$xiscoracao = "XIS CORACAO -> R$ 40.00";
+	function show12() {
+		global $xiscoracao;
+		echo $xiscoracao;
+}
+show12();
+echo "<br/>";
+$xissalada = "XIS SALADA -> R$ 40.00";
+	function show13() {
+		global $xissalada;
+		echo $xissalada;
+}
+show13();
+echo "<br/>";
+$xiscomfritas = "QUALQUER XIS CITADO ACIMA COM FRITAS -> R$ 50.00";
+	function show14() {
+		global $xiscomfritas;
+		echo $xiscomfritas;
+}
+show14();
+echo "<br/>";
+$pizzacalabresa = "PIZZA CALABRESA -> R$ 50.00";
+	function show15() {
+		global $pizzacalabresa;
+		echo $pizzacalabresa;
+}
+show15();
+echo "<br/>";
+$pizzacoracao = "PIZZA CORACAO -> R$ 50.00";
+	function show16() {
+		global $pizzacoracao;
+		echo $pizzacoracao;
+}
+show16();
+echo "<br/>";
+$pizzamarguerita = "PIZZA MARGUERITA -> R$ 50.00";
+	function show17() {
+		global $pizzamarguerita;
+		echo $pizzamarguerita;
+}
+show17();
+echo "<br/>";
+$pizzaportuguesa = "PIZZA PORTUGUESA -> R$ 50.00";
+	function show18() { 
+		global $pizzaportuguesa;
+		echo $pizzaportuguesa;
+}
+show18();
+echo "<br/>";
+$sopadecapeletti = "SOPA DE CAPELETTI -> R$ 27.00";
+	function show19() {
+		global $sopadecapeletti;
+		echo $sopadecapeletti;
+}
+show19();
+echo "<br/>";
+$cremedemandioquim = "CREME DE MANDIOQUIM -> R$ 27.00";
+	function show20() {
+		global $cremedemandioquim;
+		echo $cremedemandioquim;
+}
+show20();
+echo "<br/>";
+$tabuadefilemignon = "TABUA DE FILE MIGNON -> R$ 150.00";
+	function show21() {
+		global $tabuadefilemignon;
+		echo $tabuadefilemignon;
+}
+show21();
+echo "<br/>";
+$tabuadefilemignonacebolado = "TABUA DE FILE MIGNON ACEBOLADO -> R$ 150.00";
+	function show22() {
+		global $tabuadefilemignonacebolado;
+		echo $tabuadefilemignonacebolado;
+}
+show22();
+echo "<br/>";
+$tabuadeentrecotgrelhado = "TABUA DE ENTRECOT GRELHADO -> R$ 150.00";
+	function show23() {
+		global $tabuadeentrecotgrelhado;
+		echo $tabuadeentrecotgrelhado;
+}
+show23();
+echo "<br/>";
+$tabuadealcatraamilanesa = "TABUA DE ALCATRA A MILANESA -> R$ 130.00";
+	function show24() {
+		global $tabuadealcatraamilanesa;
+		echo $tabuadealcatraamilanesa;
+}
+show24();
+echo "<br/>";
+$bauruaoprato = "BAURU AO PRATO -> R$ 140.00";
+	function show25() {
+		global $bauruaoprato;
+		echo $bauruaoprato;
+}
+show25();
+echo "<br/>";
+$fileaparmegiana = "FILE A PARMEGIANA -> R$ 140.00";
+	function show26() {
+		global $fileaparmegiana;
+		echo $fileaparmegiana;
+}
+show26();
+echo "<br/>";
+$tabuadetilapia = "TABUA DE TILAPIA -> R$ 140.00";
+	function show27() {
+		global $tabuadetilapia;
+		echo $tabuadetilapia;
+}
+show27();
+echo "<br/>";
+$tabuadesalmao = "TABUA DE SALMAO -> R$ 160.00";
+	function show28() {
+		global $tabuadesalmao;
+		echo $tabuadesalmao;
+}
+show28();
+echo "<br/>";
+$tabuadefrutosdomar = "TABUA DE FRUTOS DO MAR -> R$ 210.00";
+	function show29() {
+		global $tabuadefrutosdomar;
+		echo $tabuadefrutosdomar;
+}
+show29();
+echo "<br/>";
+$massadefrutosdomar = "MASSA DE FRUTOS DO MAR -> R$ 200.00";
+	function show30() {
+		global $massadefrutosdomar;
+		echo $massadefrutosdomar;
+}
+show30();
+echo "<br/>";
+$paellamarinarapara2pessoas = "PAELLA MARINARA (para 2 pessoas) -> R$ 200.00";
+	function show31() {
+		global $paellamarinarapara2pessoas;
+		echo $paellamarinarapara2pessoas;
+}
+show31();
+echo "<br/>";
+$paellamarinarapara3pessoas = "PAELLA MARINARA (para 3 pessoas) -> R$ 300.00";
+	function show32() {
+		global $paellamarinarapara3pessoas;
+		echo $paellamarinarapara3pessoas;
+}
+show32();
+echo "<br/>";
+$camaraonamoranga = "CAMARAO NA MORANGA -> R$ 220.00";
+	function show33() {
+		global $camaraonamoranga;
+		echo $camaraonamoranga;
+}
+show33();
+echo "<br/>";
+$camaraoamilanesaporcaocompleta = "CAMARAO A MILANESA (porcao completa) -> R$ 200.00";
+	function show34() {
+		global $camaraoamilanesaporcaocompleta;
+		echo $camaraoamilanesaporcaocompleta;
+}
+show34();
+echo "<br/>";
+$camaraoamilanesameiaporcao = "CAMARAO A MILANESA (meia porcao) -> R$ 100.00";
+	function show35() {
+		global $camaraoamilanesameiaporcao;
+		echo $camaraoamilanesameiaporcao;
+}
+show35();
+echo "<br/>";
+$bacalhau = "BACALHAU -> R$ 250.00";
+	function show36() {
+		global $bacalhau;
+		echo $bacalhau;
+}
+show36();
+echo "<br/>";
+$bacalhaunasnatas = "BACALHAU NAS NATAS -> R$ 250.00";
+	function show37() {
+		global $bacalhaunasnatas;
+		echo $bacalhaunasnatas;
+}
+show37();
+echo "<br/>";
+$paletadecordeiroaoforno = "PALETA DE CORDEIRO AO FORNO -> R$ 210.00";
+	function show38() {
+		global $paletadecordeiroaoforno;
+		echo $paletadecordeiroaoforno;
+}
+show38();
+echo "<br/>";
+$rizzotodechampignon = "RIZZOTO DE CHAMPIGNON (molho 4 queijos) -> R$ 110.00";
+	function show39() {
+		global $rizzotodechampignon;
+		echo $rizzotodechampignon;
+}
+show39();
+echo "<br/>";
+$rizzotodecamarao = "RIZZOTO DE CAMARAO -> R$ 210.00";
+	function show40() {
+		global $rizzotodecamarao;
+		echo $rizzotodecamarao;
+}
+show40();
+echo "<br/>";
+$massacomchampignon = "MASSSA COM CHAMPIGNON -> R$ 90.00";
+	function show41() {
+		global $massacomchampignon;
+		echo $massacomchampignon;
+}
+show41();
+echo "<br/>";
+$massadamae = "MASSA DA MAE (molho 4 queijos) -> R$ 90.00";	
+	function show42() { 
+		global $massadamae;
+		echo $massadamae;
+}
+show42();
+echo "<br/>";
+$massaacarbonera = "MASSA A CARBONERA -> R$ 80.00";
+	function show43() {
+		global $massaacarbonera;
+		echo $massaacarbonera;
+}
+show43();
+echo "<br/>";
+$massabolognesa = "MASSA BOLOGNESA -> R$ 80.00";
+	function show44() {
+		global $massabolognesa;
+		echo $massabolognesa;
+}
+show44();
+echo "<br/>";
+$sobremesas = "SOBREMESAS";
+	function show45() {
+		global $sobremesas;
+		echo $sobremesas;
+}
+show45();
+echo "<br/>";
+$mussedechocolate = "MUSSE DE CHOCOLATE -> R$ 16.00";
+	function show46() {
+		global $mussedechocolate;
+		echo $mussedechocolate;
+}
+show46();
+echo "<br/>";
+$tortadebombombrancogelada = "TORTA DE BOMBOM BRANCO GELADA -> R$ 16.00";
+	function show47() {
+		global $tortadebombombrancogelada;
+		echo $tortadebombombrancogelada;
+}
+show47();
+echo "<br/>";
+$petigateu = "PETIGATEU -> R$ 16.00";
+	function show48() {
+		global $petigateu;
+		echo $petigateu;
+}
+show48();
+echo "<br/>";
+$mensagemparaocliente = "ENJOY YOUR TIME IN THE BEST WAY.";
+	function show49() {
+		global $mensagemparaocliente;
+		echo $mensagemparaocliente;
+}
+show49();
+echo "<br/>";
+
